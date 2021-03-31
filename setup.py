1a325a874c8843965e7ae6d610b3d22336ba7a71
0daniel0 <33465091+0daniel0@users.noreply.github.com>
2021-03-31 16:39:08 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,6 @@
+# Basic setup.py structure from:
+# http://stackoverflow.com/questions/16981921/relative-imports-in-python-3
+
+from setuptools import setup, find_packages
+
+setup(name='get_z2', packages=find_packages())
